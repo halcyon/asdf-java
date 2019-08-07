40e1e44e14ad14085ab3067d10169c21e9a5ce6a
Cyril Lakech <1169286+clakech@users.noreply.github.com>
2019-08-07 11:40:59 +0200

    update JAVA_HOME without running asdf

--- asdf-java-wrapper.bash
+++ asdf-java-wrapper.bash
@@ -1,12 +1,10 @@
 # Credit: github.com/trustin
 asdf_java_wrapper() {
-  if \asdf "$@"; then
-    if [[ "$(\asdf current java 2>&1)" =~ "(^([-_.a-zA-Z0-9]+)[[:space:]]*\(set by.*$)" ]]; then
-      export JAVA_HOME=$(\asdf where java ${BASH_REMATCH[2]})
-    else
-      export JAVA_HOME=''
-    fi
+  if [[ "$(\asdf current java 2>&1)" =~ "(^([-_.a-zA-Z0-9]+)[[:space:]]*\(set by.*$)" ]]; then
+    export JAVA_HOME=$(\asdf where java ${BASH_REMATCH[2]})
+  else
+    export JAVA_HOME=''
   fi
 }
 
-alias asdf='asdf_java_wrapper'
+asdf_java_wrapper
