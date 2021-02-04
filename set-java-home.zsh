af7ef9252f6bce891d89719bc4d2afa20154be6a
Fernando Crespo <fernando82@gmail.com>
2021-02-04 20:12:34 -0300

    Add absolute_dir_path to set-java-home files

--- set-java-home.zsh
+++ set-java-home.zsh
@@ -1,9 +1,15 @@
+function absolute_dir_path {
+    local absolute_path
+    absolute_path="$( cd -P "$( dirname "$1" )" && pwd )"
+    echo "$absolute_path"
+}
+
 asdf_update_java_home() {
   local java_path
   java_path="$(asdf which java)"
   if [[ -n "${java_path}" ]]; then
     export JAVA_HOME
-    JAVA_HOME="$(dirname "$(dirname "$(realpath "${java_path}")")")"
+    JAVA_HOME="$(dirname "$(dirname "$(absolute_dir_path "${java_path}")")")"
   fi
 }
 
