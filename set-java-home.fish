299e0ad83f979a0312a2c2b36341d982369fc9e9
Jochen Schalanda <jochen@schalanda.name>
2021-02-08 10:00:05 +0100

    Remove surplus `dirname` in set-java-home.fish

--- set-java-home.fish
+++ set-java-home.fish
@@ -7,7 +7,6 @@ function asdf_update_java_home --on-event fish_prompt
   set --local java_path (asdf which java)
   if test -n "$java_path"
     set --local full_path (absolute_dir_path "$java_path")
-    set --local full_path_dir (dirname "$full_path")
-    set -gx JAVA_HOME (dirname "$full_path_dir")
+    set -gx JAVA_HOME (dirname "$full_path")
   end
 end
