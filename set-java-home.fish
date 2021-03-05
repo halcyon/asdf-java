c2478ac9064c881a9cf07bad5924785c0863cfdc
Matt Tanous <mtanous22@gmail.com>
2021-03-05 12:20:30 -0500

    Specify use of builtin fish realpath

--- set-java-home.fish
+++ set-java-home.fish
@@ -1,7 +1,7 @@
 function asdf_update_java_home --on-event fish_prompt
   set --local java_path (asdf which java)
   if test -n "$java_path"
-    set --local full_path (realpath "$java_path")
+    set --local full_path (builtin realpath "$java_path")
     set -gx JAVA_HOME (dirname "$full_path")
   end
 end
