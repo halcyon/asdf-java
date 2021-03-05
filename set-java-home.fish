95a950a4a11b044335c1e8925b25b2e0053956a9
Matt Tanous <mtanous22@gmail.com>
2021-03-05 12:17:29 -0500

    Use fish builtin `realpath`

--- set-java-home.fish
+++ set-java-home.fish
@@ -1,12 +1,7 @@
-function absolute_dir_path -a name
-    set --local absolute_path (cd (dirname "$name") && pwd)
-    echo "$absolute_path"
-end
-
 function asdf_update_java_home --on-event fish_prompt
   set --local java_path (asdf which java)
   if test -n "$java_path"
-    set --local full_path (absolute_dir_path "$java_path")
+    set --local full_path (realpath "$java_path")
     set -gx JAVA_HOME (dirname "$full_path")
   end
 end
