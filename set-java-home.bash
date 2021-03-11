6936bdfb3c69b4cab86bc046dd046259f724e2e9
jinmiaoluo <39730824+jinmiaoluo@users.noreply.github.com>
2021-03-11 22:29:13 +0800

    Fix the error 'already ends with semicolon' causing by the misuse of export (#119)
    * Fix error causing by the misuse of export
    
    fixes #91
    
    * Use the solution from direnv project

--- set-java-home.bash
+++ set-java-home.bash
@@ -1,25 +1,26 @@
-function absolute_dir_path {
+function _asdf_java_absolute_dir_path {
     local absolute_path
     absolute_path="$( cd -P "$( dirname "$1" )" && pwd )"
     echo "$absolute_path"
 }
 
-asdf_update_java_home() {
+function _asdf_java_update_java_home() {
   local java_path
   java_path="$(asdf which java)"
   if [[ -n "${java_path}" ]]; then
     export JAVA_HOME
-    JAVA_HOME="$(dirname "$(absolute_dir_path "${java_path}")")"
+    JAVA_HOME="$(dirname "$(_asdf_java_absolute_dir_path "${java_path}")")"
   fi
 }
 
-prompt_command() {
+function _asdf_java_prompt_command() {
   if [[ "${PWD}" == "${LAST_PWD}" ]]; then
     return
   fi
   LAST_PWD="${PWD}"
-  asdf_update_java_home
+  _asdf_java_update_java_home
 }
 
-export PROMPT_COMMAND="${PROMPT_COMMAND:+${PROMPT_COMMAND}; prompt_command}"
-export PROMPT_COMMAND="${PROMPT_COMMAND:-prompt_command}"
+if ! [[ "${PROMPT_COMMAND:-}" =~ _asdf_java_prompt_command ]]; then
+  PROMPT_COMMAND="_asdf_java_prompt_command${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+fi
