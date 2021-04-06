38ca9d0bcc72bb0f5f81236289d2ff0c80f78c53
Wessel van Norel <wessel.van.norel@delgurth.com>
2021-04-06 17:43:02 +0200

    Update java home on zsh using buildin function, based on https://github.com/halcyon/asdf-java/pull/136, with review comment of @halcyon

--- set-java-home.zsh
+++ set-java-home.zsh
@@ -1,15 +1,9 @@
-function absolute_dir_path {
-    local absolute_path
-    absolute_path="$( cd -P "$( dirname "$1" )" && pwd )"
-    echo "$absolute_path"
-}
-
 asdf_update_java_home() {
   local java_path
   java_path="$(asdf which java)"
   if [[ -n "${java_path}" ]]; then
     export JAVA_HOME
-    JAVA_HOME="$(dirname "$(absolute_dir_path "${java_path}")")"
+    JAVA_HOME="$(dirname "$(dirname "${java_path:A}")")"
   fi
 }
 
