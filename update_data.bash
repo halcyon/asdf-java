02b4fb3e53f10002cc35bea9da803b2db61090b7
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-01-06 07:36:36 -0800

    Support multiple release types and EA builds (#179)
    * Support multiple release types and EA builds
    * Improve iteration
    * Keep files without release type backward compatible.

--- update_data.bash
+++ update_data.bash
@@ -5,6 +5,7 @@ set -Euo pipefail
 # See https://joschi.github.io/java-metadata/ for supported values
 LIST_OS="linux macosx"
 LIST_ARCH="x86_64 aarch64 arm32-vfp-hflt"
+LIST_RELEASE_TYPE="ga ea"
 
 DATA_DIR="./data"
 
@@ -16,29 +17,36 @@ fi
 function metadata_url {
 	local os=$1
 	local arch=$2
+	local release=$3
 
-	echo "https://joschi.github.io/java-metadata/metadata/ga/${os}/${arch}.json"
+	echo "https://joschi.github.io/java-metadata/metadata/${release}/${os}/${arch}.json"
 }
 
 function fetch_metadata {
 	local os=$1
 	local arch=$2
-	local url
-	url=$(metadata_url "$os" "$arch")
+	local release=$3
 
 	local args=('-s' '-f' '--compressed' '-H' "Accept: application/json")
 	if [[ -n "${GITHUB_API_TOKEN:-}" ]]; then
 		args+=('-H' "Authorization: token $GITHUB_API_TOKEN")
 	fi
 
-	curl "${args[@]}" -o "${DATA_DIR}/jdk-${os}-${arch}.json" "${url}"
+	local url
+	url=$(metadata_url "$os" "$arch" "$release")
+	curl "${args[@]}" -o "${DATA_DIR}/jdk-${os}-${arch}-${release}.json" "${url}"
 }
 
 for OS in $LIST_OS
 do
 	for ARCH in $LIST_ARCH
 	do
-		fetch_metadata "$OS" "$ARCH"
+		for RELEASE_TYPE in $LIST_RELEASE_TYPE
+		do
+			fetch_metadata "$OS" "$ARCH" "$RELEASE_TYPE"
+		done
+		cat "${DATA_DIR}/jdk-${OS}-${ARCH}"-*.json | jq -s 'add' > "${DATA_DIR}/jdk-${OS}-${ARCH}-all.json"
+		ln -s "jdk-${OS}-${ARCH}-ga.json" "${DATA_DIR}/jdk-${OS}-${ARCH}.json"
 	done
 done
 
@@ -50,5 +58,5 @@ RELEASE_QUERY='.[]
 for FILE in "${DATA_DIR}"/*.json
 do
 	TSV_FILE="$(basename "${FILE}" .json).tsv"
- 	jq -r "${RELEASE_QUERY}" "${FILE}" | sort -V > "${DATA_DIR}/${TSV_FILE}"
+	jq -r "${RELEASE_QUERY}" "${FILE}" | sort -V > "${DATA_DIR}/${TSV_FILE}"
 done
